254bb1592aca3b1f47d535e911a88e1b7727b9ec
nandhinianand <nandhinianandp@gmail.com>
2017-02-16 17:26:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/Article-1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article-1.html'));
 });
